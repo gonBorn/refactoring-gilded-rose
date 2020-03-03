f9b2f20b2011dc2e5b82e4c56329c75822dbac4a
gonborn <gonBorn@163.com>
2020-03-03 17:10:01 +0800

    [duzeyan] refactor: extract method to solve long method and move method to class Item to solve feature envy

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,54 +9,8 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            if (!"Aged Brie".equals(item.name)
-              && !"Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
-                if (item.quality > 0) {
-                    if (!"Sulfuras, Hand of Ragnaros".equals(item.name)) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-
-                    if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
-                        if (item.sellIn < 11) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-
-                        if (item.sellIn < 6) {
-                            if (item.quality < 50) {
-                                item.quality = item.quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!"Sulfuras, Hand of Ragnaros".equals(item.name)) {
-                item.sellIn = item.sellIn - 1;
-            }
-
-            if (item.sellIn < 0) {
-                if (!"Aged Brie".equals(item.name)) {
-                    if (!"Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
-                        if (item.quality > 0) {
-                            if (!"Sulfuras, Hand of Ragnaros".equals(item.name)) {
-                                item.quality = item.quality - 1;
-                            }
-                        }
-                    } else {
-                        item.quality = 0;
-                    }
-                } else {
-                    if (item.quality < 50) {
-                        item.quality = item.quality + 1;
-                    }
-                }
-            }
+            item.updateItem();
         }
     }
+
 }
--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -18,4 +18,83 @@ public class Item {
     public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
+
+    void updateItem() {
+        updateQuality();
+
+        updateSellIn();
+
+        updateQualityAfterExpiration();
+    }
+
+    private void updateQualityAfterExpiration() {
+        if (isExpired()) {
+            if (!isAgedBrie()) {
+                if (!isBackstage()) {
+                    if (quality > 0) {
+                        if (!isSulfuras()) {
+                            quality = quality - 1;
+                        }
+                    }
+                } else {
+                    quality = 0;
+                }
+            } else {
+                if (quality < 50) {
+                    quality = quality + 1;
+                }
+            }
+        }
+    }
+
+    private boolean isExpired() {
+        return sellIn < 0;
+    }
+
+    private void updateSellIn() {
+        if (!isSulfuras()) {
+            sellIn = sellIn - 1;
+        }
+    }
+
+    private void updateQuality() {
+        if (!isAgedBrie()
+          && !isBackstage()) {
+            if (quality > 0) {
+                if (!isSulfuras()) {
+                    quality = quality - 1;
+                }
+            }
+        } else {
+            if (quality < 50) {
+                quality = quality + 1;
+
+                if (isBackstage()) {
+                    if (sellIn < 11) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+
+                    if (sellIn < 6) {
+                        if (quality < 50) {
+                            quality = quality + 1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private boolean isSulfuras() {
+        return "Sulfuras, Hand of Ragnaros".equals(name);
+    }
+
+    private boolean isBackstage() {
+        return "Backstage passes to a TAFKAL80ETC concert".equals(name);
+    }
+
+    private boolean isAgedBrie() {
+        return "Aged Brie".equals(name);
+    }
 }
